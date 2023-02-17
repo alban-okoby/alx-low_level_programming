ce70d45b7b5fe2b2ac3acb5ea061cb2b071d0ce2
alban-okoby <albanokoby225@gmail.com>
2023-02-16 16:42:48 -0800

    hello, puts 🎉

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-
 #include <stdio.h>
-
-/** 
-* 
-*Return: 0 (when success) 
-*/
+/**
+ * main - the entry point
+ *
+ *  Return: 0 (when done)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
