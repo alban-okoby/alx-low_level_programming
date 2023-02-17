689129f0c35eb751f90d2a2c10fa6329bf211d36
alban-okoby <albanokoby225@gmail.com>
2023-02-17 15:27:29 -0800

    Print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main : this program print the alphabet in lowercase
+ * Return : always 0
+ */
+int myStr;
+	for (myStr = 97; myStr <= 122; myStr++)
+	{
+	putchar(myStr);
+	}
+	putchar(10);
+	return (0);
+}
+
+
