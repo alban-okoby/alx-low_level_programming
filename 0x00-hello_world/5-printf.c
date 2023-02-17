900d01b6444dc3c53a6d86c3b258b31cfebb693f
alban-okoby <albanokoby225@gmail.com>
2023-02-16 17:11:34 -0800

    Hello, printf 🎉

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - the entry point
  *
- *  Return: 0 (when done)
+ *  Return: 0 (when done ✔)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
