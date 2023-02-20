a65c8a2318ff5a05ad642b0755b7098b7fcaa072
alban-okoby <albanokoby225@gmail.com>
2023-02-20 11:51:45 -0800

    Patience, persistence and perspication make an unbreatable combination for success ✔

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Description 'this program print all possible combinations of signgle-digit numbers'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int k;
+	for(k = 48; k<= 57; k++)
+	{
+	putchar(k)
+		if (k != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
