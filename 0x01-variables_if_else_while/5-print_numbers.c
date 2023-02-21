14bed9bb6163ac1d855983c12116886e7452637e
alban-okoby <albanokoby225@gmail.com>
2023-02-20 17:02:48 -0800

    Let's celebrate another answered task with betty style 😊

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print all digit numbers of base 10
+ * starting from 0, follow by a new line'
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 11)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
