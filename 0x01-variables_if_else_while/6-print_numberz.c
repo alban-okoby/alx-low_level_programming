5a50288eff3a5d617132c687a82e7bff8714e509
alban-okoby <albanokoby225@gmail.com>
2023-02-20 16:55:07 -0800

    Betty coding style is now so far 😎

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Print all single digit number of base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int chaine;
+
+	for (chaine = 48; chaine <= 57; chaine++)
+	{
+		putchar(str);
+	}
+	putchar(10);
+	return (0);
+}
