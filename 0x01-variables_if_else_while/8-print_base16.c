ab160deb3bec64bfd8b0948f4621dda6f120c64b
alban-okoby <albanokoby225@gmail.com>
2023-02-20 16:30:26 -0800

    more tasks

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Print base program
+ *
+ * Return : Always 0
+ */
+int main(void)
+{
+	int nbre = 48;
+
+	while (nbre <= 102)
+	{
+		putchar(nbre);
+		if (nbre == 57)
+			nbre += 39;
+		++nbre;
+	}
+	putchar('\n');
+
+	return (0);
+}
