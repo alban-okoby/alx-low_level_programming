b5e80f4e810391ae13fe8b456f762de36de93a86
alban-okoby <albanokoby225@gmail.com>
2023-02-22 08:56:52 -0800

    task 5

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Chack for lowercase caracter
+ * Description: lowercase caracter
+ * @car: is an integer
+ * Return: 0 or 1;
+ */
+int _islower(int car)
+{
+	if (car < 123 && car > 98)
+		retrun (1);
+	return (0);
+}
