7a7de2f6f95741b90e0a7b65355d76e7cd138dfd
alban-okoby <albanokoby225@gmail.com>
2023-02-24 17:56:38 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main -> print if integer is negative or positive
+ * main - print if integer is negative or positive
  * Return: 0
  */
 int main(void)
