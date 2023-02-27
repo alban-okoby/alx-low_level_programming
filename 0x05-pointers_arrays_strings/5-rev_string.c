b5d6c23e070354cabe74b69abda546c645912f2e
alban-okoby <albanokoby225@gmail.com>
2023-02-27 11:13:31 -0800

    A good engineer thinks in reverse and asks himself about the stylistic consequences

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * Description: - This function print reverse string
+ * @s: param
+ *
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int i = 0, l;
+
+	char result;
+
+	l = strlen(s);
+
+	while (i < (l / 2))
+	{
+		k = s[i];
+		s[i] = s[l - (i + 1)];
+		s[l - (i + 1)] = result;
+
+		i++;
+	}
+}
