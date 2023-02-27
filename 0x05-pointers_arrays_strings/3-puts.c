58fedb4a9a2cb60f17826473090dc8399c26f0a5
alban-okoby <albanokoby225@gmail.com>
2023-02-27 10:40:32 -0800

    tas 3 done ✔

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _puts: - Entry point of function
- *@str: length param 
- * description: this function prints a string, followed by a line
- * to stdout
- * Return: always 0
+ * _puts - Entry point
+ * Description: - This function ..
+ * @str: param
+ *
+ * Return: Always 0
  */
-
 void _puts(char *str)
 {
-	printf("%s\n", str);	
+	printf("%s\n", str);
 }
