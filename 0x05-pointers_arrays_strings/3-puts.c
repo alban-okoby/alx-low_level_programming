7e01884fbb94ecf7fcad319dcbd20ece15909ca0
alban-okoby <albanokoby225@gmail.com>
2023-02-27 09:48:15 -0800

    I do not fear computers. I fear the lack of them ✨

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts: - Entry point of function
+ *@str: length param 
+ * description: this function prints a string, followed by a line
+ * to stdout
+ * Return: always 0
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);	
+}
