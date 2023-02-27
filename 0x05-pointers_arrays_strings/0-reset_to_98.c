440572cc442cc277ee557d8164fb3ba52173d49c
alban-okoby <albanokoby225@gmail.com>
2023-02-27 06:10:08 -0800

    reset to 98 function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description - This function print to a an int as
+ * parameter and updates the value ot points to to 98
+ *
+ * Return: Always 0
+ */
+int reset_to_98(int *n)
+{
+	*n = 98;
+}
