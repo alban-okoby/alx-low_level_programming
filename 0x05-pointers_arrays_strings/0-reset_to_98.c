911f36f69d0ba92a68d6941df2734aa41c66282a
alban-okoby <albanokoby225@gmail.com>
2023-02-27 06:41:56 -0800

    tast 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,13 @@
 
 /**
  * reset_to_98 - Entry point
- *
- * Description - This function print to a an int as
+ * Description: - This function print to a an int as
  * parameter and updates the value ot points to to 98
+ * @n: param
  *
  * Return: Always 0
  */
-int reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
