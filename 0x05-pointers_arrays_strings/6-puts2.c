5f81b339101cf4a5bfdb2d044529fd6a86ed9f8a
alban-okoby <albanokoby225@gmail.com>
2023-02-27 11:55:22 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts2 - Entry point
+ * Description: - This function print ..
+ *@str: param
+ * Return: Always 0
+ */
+int puts2(char *str)
+{
+	int i, l;
+
+	l = strlen(str);
+
+	for (i = 0; i < l; i++)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
