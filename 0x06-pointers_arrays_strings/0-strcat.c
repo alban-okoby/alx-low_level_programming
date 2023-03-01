377a6057f6761e3f5a610cb8ce8823ca0d13456b
alban-okoby <albanokoby225@gmail.com>
2023-03-01 15:15:19 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,10 +10,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j, k;
-	i = 0; k = 0; j = 0;
+	i = 0;
+	j = 0;
+	k = 0;
 
 	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
 		j++;
 	for (k = 0; k < j; k++)
 		*(dest + i + k) = *(src + k);
