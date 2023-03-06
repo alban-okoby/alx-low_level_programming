80aa5315b4e8120e0716457220299dc8bb63f457
alban-okoby <albanokoby225@gmail.com>
2023-03-06 11:27:47 -0800

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string
+ * @needle: the first occurence finded
+ * Return: pointer or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int k, l;
+
+	if (needle[0] == '\0')
+		return (haystack);
+	for (k = 0; needle[k] != '\0'; k++)
+	{
+		if (haystack[k] == needle[0])
+		{
+			for (l = 0; needle[l] != '\0'; l++)
+			{
+				if (haystack[k + l] != needle[l])
+					break;
+			}
+		}
+		if (needle[l] == '\0')
+			return (haystack + k);
+	}
+	return ('\0');
+}
+
