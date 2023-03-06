8dd1347f5d7cb9834184a16769572002dd381108
alban-okoby <albanokoby225@gmail.com>
2023-03-06 02:29:25 -0800

    init project

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory
+ * @s: space
+ * @b: bytes
+ * @n: memory area
+ * Return: pointer to s string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a = 0;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
+
