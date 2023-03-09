8db93e82310999159d9bcebcc52f2541ed3a4058
alban-okoby <albanokoby225@gmail.com>
2023-03-08 20:25:39 -0800

    Lastest optional challenge

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * is_palindrome - entry point
+ * description: print palindrome
+ * @s: param -> number
+ * Return: 1 if is palindrome else 0
+ */
+
+int palindrome(char *s)
+{
+	if (*s != '\0')
+	{
+		if (*s == change(s))
+		{
+			s += 1;
+			return (is_palindrome(s));
+		}
+		else
+			return (0)
+	}
+	else
+		return (1);
+}
+
+/**
+ * compare - entry print
+ * @s: param -> string
+ * Return: number -> integer
+ */
+
+char compare(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	s++;
+	compare(s);
+	s--;
+	return (*s);
+}
