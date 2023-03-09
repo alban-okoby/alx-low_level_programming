e69b85ce5e7da56357cfdeeb5e5fd68a134e7008
alban-okoby <albanokoby225@gmail.com>
2023-03-09 14:09:27 -0800

    lastest optionnal challenge

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
 /**
+ * first - entry point
+ * @a: string
+ * @l: number -> integer
+ * Return: ✔
+ */
+
+int first(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (first(a + 1, l + 1));
+}
+
+
+/**
+ * second - entry point
+ * @a: string
+ * @l: number -> integer
+ * Return: ✔
+ */
+
+int second(char *a, int l)
+{
+	if (*a != *(a + 1))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (second(a + 1, l - 2));
+}
+
+/**
  * is_palindrome - entry point
  * description: print palindrome
  * @s: param -> number
@@ -9,13 +40,8 @@
 
 int is_palindrome(char *s)
 {
-	int i;
-	int taille = strlen(s);
+	int l;
 
-	for (i = 0; i < taille / 2; i++)
-	{
-		if (s[i] != s[taille -  i - 1])
-			return (0);
-	}
-	return (1);
+	l = first(s, 0);
+	return (second(s, l));
 }
