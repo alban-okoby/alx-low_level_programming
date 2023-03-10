23aee3382c3d7a59aa5140f75faff23ef45f01ea
alban-okoby <albanokoby225@gmail.com>
2023-03-10 11:04:11 -0800

    Second challenge ✔

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argv: the offset value
+ * @argc: count of the offset
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unsed)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+	return (0);
+}
