96ec1d1db2a154f947140443556a78a7079dcba9
alban-okoby <albanokoby225@gmail.com>
2023-03-13 12:30:24 -0700

    Optional last challenge

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,49 +8,69 @@ of string (words)
 *@str: parameter -> string
 *Return: NULL if str == NULL or str == ""
 */
+int count_word(char *s)
+{
+	int result, c, x;
+
+	result = 0;
+	x = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			result = 0;
+		else if (result == 0)
+		{
+			result = 1;
+			x++;
+		}
+	}
+
+	return (x);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: return pointer to the array of strings
+ */
 char **strtow(char *str)
 {
-	int j = 0,f = 0, k = 0, s = 0, u = 0;
-	char **tableau;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	if (str == NULL)
-	{
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
 		return (NULL);
-	}
-	else
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
 	{
-		while (str[j] != '\0')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			if(str[j] == ' ')
+			if (c)
 			{
-				j++;
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
 		}
-		tableau = malloc(j * sizeof(char) + 1);
-		if (tableau == NULL)
-		{
-			return (NULL);
-		}
-		for (f = 0; f <= j; j++)
-		{
-			u = s;
-			while (str[s] != ' ')
-                	{       
-                        	s++;
-                	}
-			tableau[f] = malloc((s + 1) * sizeof(char));
-			if (tableau[f] == NULL)
-			{
-				return (NULL);
-			}
-			for (k = 0; k <= s; k++)
-			{
-				tableau[f][k] = str[u + k];
-			}
-			tableau[f][k +1] = '\0';
-			s++;
-		}
-		tableau[f + 1][1] = '\0';
-		return (tableau);
+		else if (c++ == 0)
+			start = i;
 	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
