2393a1d2b4740a8c6f3f9b137dfaa7a739fb11b0
alban-okoby <albanokoby225@gmail.com>
2023-03-13 02:37:34 -0700

    challenge 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,8 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[] __attribute__((unsed)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	if (argc > 0)
-		printf("%d\n", argc - 1);
-	return (0);
+	printf("%d\n", argc - 1);
+return (0);
 }
