6c61930ef136919c22a212400d351d923821588d
alban-okoby <albanokoby225@gmail.com>
2023-03-14 11:45:32 -0700

    challenge 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,10 +8,19 @@
  * Return: always 0
  */
 
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
-	(void)argv;
+	int i;
 
-	printf("%d\n", argc - 1);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
 	return (0);
 }
