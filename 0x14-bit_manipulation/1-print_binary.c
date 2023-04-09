3677bd59dd0aae4d8fa10f91fe21710290c6db2e
alban-okoby <albanokoby225@gmail.com>
2023-04-09 16:55:30 -0700

    Print the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,6 +2,18 @@
 #include <unistd.h>
 
 /**
+ * _putchar - writes a character to stdout
+ *
+ * @c: the character to write
+ *
+ * Return: On success, returns the number of characters
+ * written, otherwise -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
  * print_binary - Entry ppoint
  * @n: number to be printed in binary
  */
