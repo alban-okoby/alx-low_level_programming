1159262cec18ce174c9a3052017f6ad3436ec64f
alban-okoby <albanokoby225@gmail.com>
2023-04-09 13:08:46 -0700

    Fixe read_filetext issu

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,36 @@
 #include "main.h"
-/*
- * read_textfile - Reads a text file and prints it to standard output
- * @filename: The name of the file to read
- * @letters: The number of letters to read and print
- *
- * Return: The actual number of letters read and printed, or 0 on failure
+#include <stdlib.h>
+
+/**
+ ** read_textfile - Entry point
+ * @filename: the name of the file
+ * @letters: number of letters
+ * Return: number of bytes
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if (filename == NULL) {
-        return 0;
-    }
+	ssize_t open_op, read_rd, byte_wr;
+	char *buffer;
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
+	if (filename == NULL)
+		return (0);
 
-    char *buf = malloc(sizeof(char) * (letters + 1));
-    if (buf == NULL) {
-        close(fd);
-        return 0;
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    ssize_t bytes_read = read(fd, buf, letters);
-    if (bytes_read == -1) {
-        free(buf);
-        close(fd);
-        return 0;
-    }
+	open_op = open(filename, O_RDONLY);
+	read_rd = read(open_op, buffer, letters);
+	byte_wr = write(STDOUT_FILENO, buffer, read_rd);
 
-    buf[bytes_read] = '\0';
+	if (open_op == -1 || read_rd == -1 || byte_wr == -1 || byte_wr != read_rd)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-    if (bytes_written == -1 || (size_t) bytes_written != bytes_read) {
-        free(buf);
-        close(fd);
-        return 0;
-    }
+	free(buffer);
+	close(open_op);
 
-    free(buf);
-    close(fd);
-    return bytes_written;
+	return (byte_wr);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,9 @@
 #ifndef _FILE_H
 #define _FILE_H
 
-// imports all sytem librairies
+/*
+ *imports all sytem librairies
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -10,7 +12,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-// All files functions
+/* All files functions */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
