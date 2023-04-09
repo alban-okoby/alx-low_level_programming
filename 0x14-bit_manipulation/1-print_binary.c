99bfc35a419cc0f9a8f87f502902a5c62359f442
alban-okoby <albanokoby225@gmail.com>
2023-04-09 16:44:45 -0700

    Print the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,25 +12,7 @@ int _putchar(char c)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int bit_mask = 1;
-	int bit_number = 0;
-
-	/* Find the number of bits in n */
-	while ((n >> bit_number) > 0)
-		bit_number++;
-
-	/* Print each bit starting from the most significant bit */
-	while (bit_number--)
-	{
-		if (bit_mask & n)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		bit_mask <<= 1; /* Shift mask to the next bit */
-	}
-
-	/* If n is 0, still print a 0 */
-	if (bit_number < 0)
-		_putchar('0');
+	if ( 2 <= n)
+		print_binary( 1 << n);
+	_putchar('0' + (1 & n));
 }
