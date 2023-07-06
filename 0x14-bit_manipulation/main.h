4f8b735c0f39425395dcea913ed2c5e9137b7f80
alban-okoby <albanokoby225@gmail.com>
2023-07-06 14:37:20 -0700

    project 0 and 1

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,18 @@
+#ifndef _ALX_HOLBERTON_H
+#define _ALX_HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+/* A putchar function */
+int _putchar(char c);
+
+
+#endif /* _ALX_HOLBERTON_H */
