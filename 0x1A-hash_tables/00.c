d843a6ce39c316f78301a0a6f30b678beb6d87a1
alban-okoby <albanokoby225@gmail.com>
2023-09-09 10:16:57 -0700

    Fix issue of >>> ht = {}

--- 0x1A-hash_tables/00.c
+++ 0x1A-hash_tables/00.c
@@ -1,17 +1,20 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
  * hash_table_create - Creates a hash table
  * @size: The size of the array
  *
- * Return: NULL on error
- *         Otherwise - a pointer to the new hash table
+ * Return: A pointer to the newly created hash table,
+ *         or NULL if something went wrong
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *new_table = NULL;
 	unsigned long int i;
 
+	if (size < 1 || new_table == NULL)
+		return (NULL);
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
@@ -19,7 +22,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
+	{
+		/*free(new_table);*/
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
 
