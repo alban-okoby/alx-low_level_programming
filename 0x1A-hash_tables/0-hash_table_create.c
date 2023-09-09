c91df94aed6ef1264f452e43625fce589f9033a2
alban-okoby <albanokoby225@gmail.com>
2023-09-09 10:07:13 -0700

    Issue of 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = NULL;
 	unsigned long int i;
 
-	if (size < 1)
+	if (size < 1 || new_table == NULL)
 		return (NULL);
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
@@ -23,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
-		free(new_table);
+		/*free(new_table);*/
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
