478964cd1b4a6f79c7e82c81ccbfa2b02c2c3d8e
alban-okoby <albanokoby225@gmail.com>
2023-10-25 03:09:17 -0700

    Create header & main files

---
+++ 0x1C-makefiles/header.h
@@ -0,0 +1,10 @@
+#ifndef _MAKER_
+#define _MAKER_
+
+/* Import librairies */
+#include <stdio.h>
+#include <stdlib.io>
+
+void call_print_school(void);
+
+#endif
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "header.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always main function
+ */
+int main(void)
+{
+	call_print_school();
+	return(EXIT_SUCCESS);
+}
