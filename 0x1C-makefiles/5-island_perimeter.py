157be489bd2290d81fff0b32f2b6e33f53f6cacb
alban-okoby <albanokoby225@gmail.com>
2023-10-25 03:54:38 -0700

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Function that calculate the perimeter of an island described in the grid
+
+    Args:
+    grid (List[List[int]]): A list of lists representing the grid
+
+    Returns:
+    int: The perimeter of the island
+
+    Example:
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    island_perimeter(grid) should return 12
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                # Check if there is land adjacent to the current cell
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
