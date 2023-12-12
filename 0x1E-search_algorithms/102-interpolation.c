8f31d3dcf810b4d90e9b14a3191717d998b6a79f
alban-okoby <albanokoby225@gmail.com>
2023-12-12 05:13:56 -0800

    Revert "All betty well done ✅"
    This reverts commit 31a50497118451e73f113e0673363af8ba18739f.

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,16 +14,14 @@
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0;
-	size_t high = size - 1;
+        size_t high = size - 1;
 
 	if (array == NULL)
 		return (-1);
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
-		size_t rightResult = (array[high] - array[low]) * (value - array[low]);
-		size_t pos = low + (((double)(high - low) / (rightResult);
-
+		size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
